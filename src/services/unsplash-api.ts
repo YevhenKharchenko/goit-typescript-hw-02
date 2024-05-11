24cbf1e3990c1fda532b74a36a289a278fd68822
YevhenKharchenko <evgenvalerych@gmail.com>
2024-05-12 00:18:23 +0300

    update

--- src/services/unsplash-api.ts
+++ src/services/unsplash-api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
-interface Response {
+interface IResponse {
   total_pages: number;
   results: IImage[];
 }
@@ -11,8 +11,8 @@ interface Response {
 export const requestImages = async (
   value: string,
   page: number
-): Promise<Response> => {
-  const response = await axios.get<Response>(
+): Promise<IResponse> => {
+  const response = await axios.get<IResponse>(
     `https://api.unsplash.com/search/photos?client_id=${apiKey}&page=${page}&per_page=12&orientation=landscape&query=${value}`
   );
 
